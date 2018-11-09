198b50404277d27a893c92ad329dc1a493ba8122
SurAritra <aritrasur@gmail.com>
2018-11-09 17:35:15 +0530

    Create Setup_and_link_extractor_script.sh

---
+++ Setup_and_link_extractor_script.sh
@@ -0,0 +1,7 @@
+echo "Code refference : https://github.com/ibnesayeed/linkextractor "
+echo "Setup requires : cloning gihut repository and getting into the folder checkout the version"
+
+git clone https://github.com/ibnesayeed/linkextractor.git
+cd linkextractor
+git checkout demo
+
